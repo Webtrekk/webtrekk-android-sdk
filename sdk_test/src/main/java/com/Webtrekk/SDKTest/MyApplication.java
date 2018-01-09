aaa1671b8e6e5f790a4807837cf5e844891a963a
arsen.vartbaronov <arsen.vartbaronov@webtrekk.com>
2018-01-09 09:38:40 +0100

    AppsFlyer integration
    timeout
    
    MT-2021

--- sdk_test/src/main/java/com/Webtrekk/SDKTest/MyApplication.java
+++ sdk_test/src/main/java/com/Webtrekk/SDKTest/MyApplication.java
@@ -18,7 +18,30 @@
 
 package com.Webtrekk.SDKTest;
 
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.util.JsonReader;
+
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.webtrekk.webtrekksdk.Request.RequestProcessor;
+import com.webtrekk.webtrekksdk.Utils.HelperFunctions;
+import com.webtrekk.webtrekksdk.Utils.WebtrekkLogging;
 import com.webtrekk.webtrekksdk.WebtrekkApplication;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Map;
 
 /**
  * Created by user on 11/03/15.
@@ -36,5 +59,73 @@ public class MyApplication extends WebtrekkApplication {
     public void onCreate() {
         super.onCreate();
         mSDKManager.setup();
+
+        AppsFlyerConversionListener conversionDataListener =
+                new AppsFlyerConversionListener() {
+
+                    @Override
+                    public void onInstallConversionDataLoaded(Map<String, String> map) {
+                        final String status = map.get("af_status");
+                        final String media_source = map.get("media_source");
+                        WebtrekkLogging.log("media_source: " + media_source);
+                        WebtrekkLogging.log("status: " + status);
+                        if (status != null && status.equals("Non-organic") && media_source != null) {
+                            final PostInstallSender sender = new PostInstallSender(MyApplication.this);
+                            sender.send(media_source);
+                            //sender.send(media_source, getAdvID());
+                        }
+                        WebtrekkLogging.log("onInstallConversionDataLoaded. Value: " + getPlainString(map));
+                    }
+
+                    @Override
+                    public void onInstallConversionFailure(String s) {
+                        WebtrekkLogging.log("onInstallConversionFailure. Value: " + s);
+                    }
+
+                    @Override
+                    public void onAppOpenAttribution(Map<String, String> map) {
+                        WebtrekkLogging.log("onAppOpenAttribution. Value: " + getPlainString(map));
+                    }
+
+                    @Override
+                    public void onAttributionFailure(String s) {
+                        WebtrekkLogging.log("onAttributionFailure. Value: " + s);
+                    }
+
+                    @NonNull
+                    private String getPlainString(@NonNull Map<String, String> map){
+                        String mapPlainStr = "";
+                        for (Map.Entry<String, String> entry : map.entrySet()){
+                            mapPlainStr += " key:"+ entry.getKey()+" value:" + entry.getValue();
+                        }
+
+                        return mapPlainStr;
+                    }
+                };
+        AppsFlyerLib.getInstance().init("SMBb7b3DFRJ2T5i8iETMcD", conversionDataListener, getApplicationContext());
+        AppsFlyerLib.getInstance().startTracking(this);
+        AppsFlyerLib.getInstance().setDebugLog(true);
+    }
+
+
+    private String getAdvID(){
+        AdvertisingIdClient.Info adInfo = null;
+        try {
+            adInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
+
+        } catch (IOException e) {
+            // Unrecoverable error connecting to Google Play services (e.g.,
+            // the old version of the service doesn't support getting AdvertisingId).
+
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Google Play services is not available entirely.
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return adInfo.getId();
     }
 }
